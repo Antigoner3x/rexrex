7d4dfbeaa4b53913d69b7713f30034b95518620a
Antigoner3x <stephanie.h.lauk@gmail.com>
2017-03-24 16:23:38 -0700

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,7 +1,7 @@
 import json
 
 
-jsondata = r'C:\Users\steph\Documents\json\x00report.json'
+jsondata = r'C:\Users\json\x00report.json'
 
 data_s = [] #to string
 data_o = [] #to object
@@ -21,12 +21,9 @@ class openJson(object):
             
         #print data_o[0]
         
-        print "Hidden Services:\n        ", data_o[0]["hiddenService"], "\n"
         print "Performed Scans:\n        ", data_o[0]["performedScans"], "\n"
         print "Simple Report:\n        ", data_o[0]["simpleReport"]['risks'], "\n"
-        print "Crawls:\n        ", data_o[0]["crawls"]["http://0-0helix.grams7enekj3fhxz.onion/"], "\n"
-        print "Date Scanned:\n        ", data_o[0]["dateScanned"], "\n"
-        print "Identifier Report: Linked Onions:\n        ", data_o[0]["identifierReport"]["linkedOnions"], "\n"
+
     
         return data_s, data_o
 
